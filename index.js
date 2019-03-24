51a2691320413e6b0b7bd42238ce1ee8dbec80cb
Gustavo F. Silva <gustafsilva@hotmail.com>
2019-03-23 23:23:42 -0300

    Adjusting actions :fire:

--- index.js
+++ index.js
@@ -26,14 +26,30 @@ function createStore(reducer) {
   };
 }
 
-/** App TODO (Reducer) */
+/** App TODO */
+// Actions Types
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+// Generators Actions
+const addTodoAction = todo => ({ type: ADD_TODO, todo });
+const removeTodoAction = id => ({ type: REMOVE_TODO, id });
+const toggleTodoAction = id => ({ type: TOGGLE_TODO, id });
+
+const addGoalAction = goal => ({ type: ADD_GOAL, goal });
+const removeGoalAction = id => ({ type: REMOVE_TODO, id });
+
 function todoReducer(state = [], action) {
   switch(action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
       return state.filter(todo => todo.id != action.id);
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map(todo => todo.id !== action.id
         ? todo
         : Object.assign({}, todo, { complete: !todo.complete })
@@ -45,9 +61,9 @@ function todoReducer(state = [], action) {
 /** APP Goal (Reducer) */
 function goalReducer(state = [], action) {
   switch(action.type) {
-    case 'ADD_GOAL':
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case 'REMOVE_GOAL':
+    case REMOVE_GOAL:
       return state.filter(goal => goal.id !== action.id);
     default:
       return state;
