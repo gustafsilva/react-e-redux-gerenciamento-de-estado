c5f5cf35502824326a08d60122d552de9e2f743a
Gustavo F. Silva <gustafsilva@hotmail.com>
2019-03-23 22:47:13 -0300

    Creating TODO reducer, responsible for capturing an action and genrating a new state.

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ function createStore(reducer) {
     dispatch
   };
 }
+
+/** App TODO (Reducer) */
+function todoReducer(state =[], action) {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo]);
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id != action.id);
+    case 'TOGGLE_TODO':
+      return state.map(todo => todo.id !== action.id
+        ? todo
+        : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+        return state;
+  }
+}
